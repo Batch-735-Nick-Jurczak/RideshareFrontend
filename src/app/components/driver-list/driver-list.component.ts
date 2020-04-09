c8e8f9a9b70d3c1833f698f780a08dbeea7d07a2
Kenneth <Kenneth.a.stratton@gmail.com>
2020-04-09 16:45:36 -0500

    current user logged in is not displayed

--- src/app/components/driver-list/driver-list.component.ts
+++ src/app/components/driver-list/driver-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, SkipSelf } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user-service/user.service';
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 import { BatchService } from 'src/app/services/batch-service/batch.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Driver } from 'src/app/models/driver';
 
 @Component({
   selector: 'app-driver-list',
@@ -19,165 +20,167 @@ import { environment } from '../../../environments/environment';
 export class DriverListComponent implements OnInit {
 
   location: string = sessionStorage.getItem("haddress") + " " + sessionStorage.getItem("hcity") + "," + sessionStorage.getItem("hstate");
-  
-mapProperties: { };
-availableCars: Array < any > =[];
-drivers: Array < any > =[];
 
+  page = 1;
+  pageSize = 5;
+  mapProperties: {};
+  availableCars: Array<any> = [];
+  drivers: Driver[] = [];
+  driverName: string;
+  driverOrigin: string;
 
-@ViewChild('map', null) mapElement: any;
-map: google.maps.Map;
+  @ViewChild('map', null)
+  mapElement: any;
+  map: google.maps.Map;
 
-constructor(private http: HttpClient, private userService: UserService) { }
+  constructor(private http: HttpClient, private userService: UserService) { }
 
-ngOnInit() {
-  this.drivers = [];
-  this.userService.getRidersForLocation1(this.location).subscribe(
-    res => {
-      //console.log(res);
-      res.forEach(element => {
-        this.drivers.push({
-          'id': element.userId,
-          'name': element.firstName + " " + element.lastName,
-          'origin': element.hAddress + " " + element.hCity + "," + element.hState,
-          'email': element.email,
-          'phone': element.phoneNumber
-        });
-      });
+  ngOnInit() {
+    this.getGoogleApi();
+    this.sleep(1000).then(() => {
+      this.userService.getRidersForBatch(1).subscribe(
+        drivers => this.drivers = this.getDistanceAndDuration(this.location, drivers));
+    })
+    // this.getGoogleApi();
+
+    this.sleep(2000).then(() => {
+      this.mapProperties = {
+        center: new google.maps.LatLng(Number(sessionStorage.getItem('lat')), Number(sessionStorage.getItem('lng'))),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapProperties);
+      // get all routes
+      // show drivers on map
+      this.showDriversOnMap(this.location, this.drivers);
     });
-  /*this.drivers.push({'id': '1','name': 'Ed Ogeron','origin':'Reston, VA', 'email': 'ed@gmail.com', 'phone':'555-555-5555'});
-  this.drivers.push({'id': '2','name': 'Nick Saban','origin':'Oklahoma, OK', 'email': 'nick@gmail.com', 'phone':'555-555-5555'});
-  this.drivers.push({'id': '3','name': 'Bobbie sfsBowden','origin':'Texas, TX', 'email': 'bobbie@gmail.com', 'phone':'555-555-5555'});
-  this.drivers.push({'id': '4','name': 'Les Miles','origin':'New York, NY', 'email': 'les@gmail.com', 'phone':'555-555-5555'});
-  this.drivers.push({'id': '5','name': 'Bear Bryant','origin':'Arkansas, AR', 'email': 'bear@gmail.com', 'phone':'555-555-5555'});*/
-  //console.log(this.drivers);
-  this.getGoogleApi();
+  }
 
-  this.sleep(2000).then(() => {
-    this.mapProperties = {
-      center: new google.maps.LatLng(Number(sessionStorage.getItem("lat")), Number(sessionStorage.getItem("lng"))),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapProperties);
-    //get all routes 
-    this.displayDriversList(this.location, this.drivers);
-    //show drivers on map
-    this.showDriversOnMap(this.location, this.drivers);
-  });
-}
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-getGoogleApi() {
-  this.http.get(`${environment.loginUri}getGoogleApi`)
-    .subscribe(
-      (response) => {
-        //console.log(response);
-        if (response["googleMapAPIKey"] != undefined) {
-          new Promise((resolve) => {
-            let script: HTMLScriptElement = document.createElement('script');
-            script.addEventListener('load', r => resolve());
-            script.src = `http://maps.googleapis.com/maps/api/js?key=${response["googleMapAPIKey"][0]}`;
-            document.head.appendChild(script);
-          });
+  /**
+   * getGoogleApi gets the API key 
+   * to be able to leverage the Google API
+   */
+  getGoogleApi() {
+    this.http.get(`${environment.loginUri}getGoogleApi`)
+      .subscribe(
+        (response) => {
+          // console.log(response);
+          if (response["googleMapAPIKey"] !== undefined) {
+            new Promise((resolve) => {
+              const script: HTMLScriptElement = document.createElement('script');
+              script.addEventListener('load', () => resolve());
+              script.src = `http://maps.googleapis.com/maps/api/js?key=${response["googleMapAPIKey"][0]}`;
+              document.head.appendChild(script);
+            });
+          }
         }
-      }
-    );
-}
-
-showDriversOnMap(origin, drivers) {
-  console.log("Origin: " + origin);
-  console.log(drivers);
-  drivers.forEach(element => {
-    var directionsService = new google.maps.DirectionsService;
-    var directionsRenderer = new google.maps.DirectionsRenderer({
-      draggable: false,
-      map: this.map
+      );
+  }
+/**
+   * showDriversOnMap displays the pin for 
+   * the location of the driver on the Google Map
+   * @param origin
+   * @param drivers
+   */
+  showDriversOnMap(origin, drivers) {
+    drivers.forEach(element => {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        draggable: false,
+        map: this.map
+      });
+      this.displayRoute(origin, element.origin, directionsService, directionsRenderer);
     });
-    this.displayRoute(origin, element.origin, directionsService, directionsRenderer);
-  });
-}
+  }
 
-
-displayRoute(origin, destination, service, display) {
-  service.route({
-    origin: origin,
-    destination: destination,
-    travelMode: 'DRIVING',
-    //avoidTolls: true
-  }, function (response, status) {
-    if (status === 'OK') {
-      display.setDirections(response);
-    } else {
-      alert('Could not display directions due to: ' + status);
-    }
-  });
-}
-
-
-displayDriversList(origin, drivers) {
-  let origins = [];
-  //set origin
-  origins.push(origin)
-
-  var outputDiv = document.getElementById('output');
-  drivers.forEach(element => {
-
-    var service = new google.maps.DistanceMatrixService;
-    service.getDistanceMatrix({
-      origins: origins,
-      destinations: [element.origin],
-      travelMode: google.maps.TravelMode.DRIVING,
-      unitSystem: google.maps.UnitSystem.IMPERIAL,
-      avoidHighways: false,
-      avoidTolls: true
+  /**
+   * displayRoute displays the route between the rider's location
+   * and each driver's location on the map
+   * @param origin
+   * @param destination
+   * @param service
+   * @param display
+   */
+  displayRoute(origin, destination, service, display) {
+    service.route({
+      origin,
+      destination,
+      travelMode: 'DRIVING',
+      // avoidTolls: true
     }, function (response, status) {
-      if (status !== 'OK') {
-        alert('Error was: ' + status);
+      if (status === 'OK') {
+        display.setDirections(response);
       } else {
-        var originList = response.originAddresses;
-        var destinationList = response.destinationAddresses;
-        var results = response.rows[0].elements;
-        //console.log(results[0].distance.text);
-        var name = element.name;
-        outputDiv.innerHTML += `<tr><td class="col">${name}</td>
-                                  <td class="col">${results[0].distance.text}</td>
-                                  <td class="col">${results[0].duration.text}</td>
-                                  <td class="col">
-                                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCentered${element.id}"> View</button>
-                                    <div class="col-lg-5">
-                                     <div class="modal" id="exampleModalCentered${element.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
-                                      <div class="modal-dialog modal-dialog-centered" role="document">
-                                          <div class="modal-content">
-                                              <div class="modal-header">
-                                                  <h5 class="modal-title" id="exampleModalCenteredLabel">Contact Info:</h5>
-                                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                     <span aria-hidden="true">×</span>
-                                                   </button>
-                                              </div>
-                                              <div class="modal-body">
-                                                  <h1>${name}</h1>
-                                                  <h3>Email: ${element.email}</h3>         
-                                                  <h3>Phone: ${element.phone}</h3>                 
-                                              </div>
-                                              <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                              </div>
-                                            </div>
-                                         </div>
-                                       </div>
-                                  </div>
-                                  <div class="col-lg-6">
-                                      <div #maps id="gmap" class="img-responsive"></div>
-                                  </div>
-                                </td></tr>`;
+        alert('Could not display directions due to: ' + status);
       }
     });
+  }
 
-  });
-}
+  /**
+   * addToModal responds to a click on a list item in the driver list
+   * It then sets component properties - driverName and driverOrigin
+   * @param driver takes the driver object 
+   */
+  addToModal(driver: Driver) {
+    this.driverName = driver.name;
+    this.driverOrigin = driver.origin;
+  }
+
+
+  /**
+   * getDistanceAndDuration computes the distance and driving duration
+   * between the current rider and each available driver.
+   * It then populates the drivers property with 
+   * driver information and the computed distance and duration
+   * for each driver
+   * @param origin
+   * @param drivers
+   */
+  getDistanceAndDuration(origin, drivers): Driver[] {
+    const origins = [];
+    // set origin
+    origins.push(origin);
+
+    const newDrivers: Driver[] = [];
+
+    drivers.forEach(element => {
+      const service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix({
+        origins,
+        destinations: [element.hAddress + " " + element.hCity + ',' + element.hState],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+        avoidHighways: false,
+        avoidTolls: false
+      }, function (response, status) {
+        if (status !== 'OK') {
+          alert('Error was: ' + status);
+        } else if (sessionStorage.getItem("userid") == element.userId) {
+
+        } else {
+          const results = response.rows[0].elements;
+          const id = element.userId;
+          const name = element.firstName + ' ' + element.lastName;
+          const origin = element.hAddress + " " + element.hCity + ',' + element.hState;
+          const distance = results[0].distance.text;
+          const time = results[0].duration.text;
+          const newDriver = new Driver({
+            id,
+            name,
+            origin,
+            distance,
+            time
+          });
+          newDrivers.push(newDriver);
+        }
+      });
+
+    });
+    return newDrivers;
+  }
 
 }
--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -230,4 +230,8 @@ export class UserService {
 	getRidersForLocation1(location: string): Observable <any>{
 		return this.http.get(this.url + 'driver/'+ location)
 	}
+
+	getRidersForBatch(batch: number): Observable <any>{
+		return this.http.get(this.url +'?batch-num='+ batch)
+	}
 }
