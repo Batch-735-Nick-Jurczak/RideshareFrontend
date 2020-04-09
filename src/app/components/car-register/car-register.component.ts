c65e0fd2ad56e1fa26d9730c6d41ce55db04674a
wan03 <38962452+wan03@users.noreply.github.com>
2020-04-09 09:37:13 -0400

    Finished improving the car-register component. Still have to finish the tests

--- src/app/components/car-register/car-register.component.ts
+++ src/app/components/car-register/car-register.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CarService } from 'src/app/services/car-service/car.service';
-import { ValidationService } from 'src/app/services/validation-service/validation.service';
-import { Car } from 'src/app/models/car';
-import { AuthService } from 'src/app/services/auth-service/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { CarService } from "src/app/services/car-service/car.service";
+import { ValidationService } from "src/app/services/validation-service/validation.service";
+import { Car } from "src/app/models/car";
 
 @Component({
-  selector: 'app-car-register',
-  templateUrl: './car-register.component.html',
-  styleUrls: ['./car-register.component.css']
+  selector: "app-car-register",
+  templateUrl: "./car-register.component.html",
+  styleUrls: ["./car-register.component.css"]
 })
 
-  /**
-   * The Car Register component
-   */
-
+/**
+ * The Car Register component
+ */
 export class CarRegisterComponent implements OnInit {
-
   /**
    * Set years as an array of numbers
    * Set userId
@@ -28,46 +24,45 @@ export class CarRegisterComponent implements OnInit {
   car: Car = new Car();
 
   /**
-   * This is constructor
-   * @param carService A dependency of a car service is injected.
-   * @param router Provides an instance of a router.
+   * This is the constructor
+   * @param carService A dependency of a car service is injected
+   * @param validationService A dependency of validation service is injected
    */
 
-  constructor(private carService: CarService, private router: Router, public validationService: ValidationService, private authService: AuthService) { }
+  constructor(
+    private carService: CarService,
+    public validationService: ValidationService
+  ) {}
 
   /**
-   * This is an OnInit function that sets the user id as the parsed string in session storage.
-   * The system will check if the user id is valid.
-   * Once validated, it will initialize the fields.
+   * This is an OnInit function that sets the initial values
    */
   ngOnInit() {
-    // this.userId = this.authService.user.userId;
-
-      let currentYear = new Date().getFullYear();
-      let availableYear = currentYear - 15;
-      for (let i = availableYear; i <= currentYear; i++) {
-        this.years.push(i);
-        this.car.year = this.years[0];
-      }
-
-  }
-
- /**
-  * @param event
-  * @returns {void}
-  */
-  changeYear(event) {
-		let option = event.target.options.selectedIndex;
-		this.car.year = this.years[option];
+    let currentYear = new Date().getFullYear();
+    let availableYear = currentYear - 15;
+    for (let i = availableYear; i <= currentYear; i++) {
+      this.years.push(i);
+      this.car.year = this.years[0];
+    }
   }
 
   /**
-   * A POST method that adds a car object to the user
+   * Gets current year from selection and applies it to the car object
+   * @param event
+   * @returns {void}
+   */
+  changeYear(event) {
+    let option = event.target.options.selectedIndex;
+    this.car.year = this.years[option];
+  }
+
+  /**
+   * Validates the number of seats as well and sends the info to
+   * the car service so the data can be persisted
    */
   addCar() {
     if (this.validationService.validateSeats(this.car.seats)) {
       this.carService.createCar(this.car, this.userId);
     }
   }
-
 }
