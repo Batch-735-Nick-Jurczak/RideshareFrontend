e13de50a90553ff4b859ba8911bdef10206e60b1
Your Name <you@example.com>
2020-04-13 19:18:22 -0500

    added validation to the contact information page.

--- src/app/components/profile-contact/profile-contact.component.ts
+++ src/app/components/profile-contact/profile-contact.component.ts
@@ -1,46 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user-service/user.service';
-import { Router } from '@angular/router';
-import { User } from 'src/app/models/user';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user-service/user.service";
+import { Router } from "@angular/router";
+import { User } from "src/app/models/user";
 
 @Component({
-  selector: 'app-profile-contact',
-  templateUrl: './profile-contact.component.html',
-  styleUrls: ['./profile-contact.component.css']
+  selector: "app-profile-contact",
+  templateUrl: "./profile-contact.component.html",
+  styleUrls: ["./profile-contact.component.css"],
 })
 export class ProfileContactComponent implements OnInit {
-
-  profileObject : User;
-  currentUser: any = '';
+  /**
+   *Created a user to hold the current user information comming from 
+   *database
+   */
+  profileObject: User;
+  /**
+   *Create variables to hold the information in the form
+   * 
+   */
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
-  success :string;
-  constructor(private router: Router, private userService: UserService) { }
+  /**
+   *Variable to hold the success or fail status that will be recieved from the database
+   */
+  success: string;
+
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {
-    this.currentUser = this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
-      this.profileObject = response;
-
-      this.firstName = this.profileObject.firstName;
-      this.lastName = this.profileObject.lastName;
-      this.email = this.profileObject.email;
-      this.phone = this.profileObject.phoneNumber;
-
-    });
-    
+    /**
+     *calling the user service to get the current users information
+     */
+    this.userService
+      .getUserById2(sessionStorage.getItem("userid"))
+      .subscribe((response) => {
+        console.log(response)
+        /**
+         *setting the current user to the local user profile
+         */
+        this.profileObject = response;
+        /**
+         *Setting the current logged in users info to the form information via ngModel.
+         */
+        this.firstName = this.profileObject.firstName;
+        this.lastName = this.profileObject.lastName;
+        this.email = this.profileObject.email;
+        this.phone = this.profileObject.phoneNumber;
+      });
   }
+  /**
+   *Function that will submit the updated changes to the database
+   */
+  updatesContactInfo({ value, valid }: { value: User; valid: Boolean }) {
 
-  updatesContactInfo(){
+    /**
+     * Updating the current users information with the information from the form.
+     */
     this.profileObject.firstName = this.firstName;
     this.profileObject.lastName = this.lastName;
     this.profileObject.email = this.email;
     this.profileObject.phoneNumber = this.phone;
-
-    this.userService.updateUserInfo(this.profileObject);
+    /**
+     * Calling the user service to update the current users information
+     */
+    let message = this.userService.updateUserInfo(this.profileObject);
+    /**
+     * updating the successtatus.
+     */
     this.success = "Updated Successfully!";
+    console.log(message);
   }
-
-
 }
