96f5949482f2710a4fdada9967709b2b6ea15e7a
Enoc Santiago <enocsanti2@gmail.com>
2020-04-15 10:52:28 -0400

    SortByName works, before adding debugger dependency

--- src/app/components/driver-list/driver-list.component.spec.ts
+++ src/app/components/driver-list/driver-list.component.spec.ts
@@ -1,12 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DriverListComponent } from './driver-list.component';
-//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppModule } from '../../app.module';
+import { Driver } from '../../models/driver';
+import { mockDrivers, mockDriversSorted } from '../../models/driver-mocked';
+
+/**
+ * Test the efficacy of sorting algorithms in driver-list.component.ts
+ * List of algorithms that are tested:
+ * sort(orderedBy: string): void; uses:
+ *    changeSortOrder();
+ *    sortByName(): void;
+ *    sortByDistance(): void;
+ *    sortByTime(): void; uses:
+ *      getMinutes(duration: string): number;
+ *    sortOrderIsLow(orderedBy: string): boolean;
+ * sleep(ms): Promise; //used by many functions
+ * getGoogleApi(): void;  Unable to test because async()'s dependency called Zone cannot be detected by Protractor
+ * addToModal(driver: Driver);
+ * getDistanceAndDuration(origin, drivers): Driver[];
+ * 
+ * 
+ * 
+*/
 
 describe('DriverListComponent', () => {
   let component: DriverListComponent;
   let fixture: ComponentFixture<DriverListComponent>;
+  let driversExpected: Driver[] = [];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,12 +37,22 @@ describe('DriverListComponent', () => {
   }));
 
   beforeEach(() => {
+    driversExpected = [...mockDriversSorted];
     fixture = TestBed.createComponent(DriverListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.drivers = [...mockDrivers];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('Sort By Name', () => {
+    component.sortOrder = 'low';
+    component.orderedBy = 'name';
+    component.sortByName();
+    //spyOn(this.dlc, 'sortByName');
+    expect(component.drivers).toEqual(driversExpected);
+    });
 });
--- src/app/models/driver-mocked.ts
+++ src/app/models/driver-mocked.ts
@@ -2,7 +2,7 @@ import { DriverProps, Driver } from './driver';
 
 export const mockDriverProps1: DriverProps = {
     id: 1,
-    name: 'minh',
+    name: 'C',
     location: '123 First Ave',
     email: 'example@example.com',
     phoneNumber: '5555555555',
@@ -12,7 +12,17 @@ export const mockDriverProps1: DriverProps = {
 
 export const mockDriverProps2: DriverProps = {
     id: 2,
-    name: 'mike',
+    name: 'A',
+    location: '123 First Ave',
+    email: 'example@example.com',
+    phoneNumber: '5555555555',
+    distance: '4 mi',
+    duration: '9 min'
+};
+
+export const mockDriverProps3: DriverProps = {
+    id: 3,
+    name: 'B',
     location: '123 First Ave',
     email: 'example@example.com',
     phoneNumber: '5555555555',
@@ -20,5 +30,5 @@ export const mockDriverProps2: DriverProps = {
     duration: '10 min'
 };
 
-export const mockDrivers: Driver[] = [ new Driver(mockDriverProps1), new Driver(mockDriverProps2) ];
-export const mockDriversSorted: Driver[] = [ new Driver(mockDriverProps2), new Driver(mockDriverProps1) ];
+export const mockDrivers: Driver[] = [ new Driver(mockDriverProps1), new Driver(mockDriverProps2), new Driver(mockDriverProps3) ];
+export const mockDriversSorted: Driver[] = [ new Driver(mockDriverProps2), new Driver(mockDriverProps3), new Driver(mockDriverProps1) ];
